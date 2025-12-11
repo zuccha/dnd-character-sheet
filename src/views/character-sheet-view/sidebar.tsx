cfa2885f0783f9f768abeb04a6c398357d78fda6
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-12-11 21:47:04 +0100

    fix: position theme button properly in sidebar

--- src/views/character-sheet-view/sidebar.tsx
+++ src/views/character-sheet-view/sidebar.tsx
@@ -1,10 +1,19 @@
-import { VStack } from "@chakra-ui/react";
+import { HStack, VStack } from "@chakra-ui/react";
 import ThemeButton from "../../theme/theme-button";
 
 export default function Sidebar() {
   return (
-    <VStack bgColor="bg.l1" borderRightWidth={1} h="100vh" w="20em">
-      <ThemeButton />
+    <VStack
+      bgColor="bg.l1"
+      borderRightWidth={1}
+      h="100vh"
+      px={4}
+      py={2}
+      w="20em"
+    >
+      <HStack justify="flex-end" w="full">
+        <ThemeButton />
+      </HStack>
     </VStack>
   );
 }
