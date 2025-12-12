fea479f821565a695fe332f0466b9f774561b04a
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-12-12 23:43:39 +0100

    feat: create dialog component

---
+++ src/ui/dialog.tsx
@@ -0,0 +1,64 @@
+import {
+  CloseButton,
+  Dialog as ChakraDialog,
+  type DialogRootProps as ChakraDialogRootProps,
+  Portal,
+} from "@chakra-ui/react";
+import Button from "./button";
+
+//------------------------------------------------------------------------------
+// Dialog
+//------------------------------------------------------------------------------
+
+export type DialogProps = Omit<ChakraDialogRootProps, "onOpenChange"> & {
+  cancelText: string;
+  confirmText: string;
+  onConfirm: () => void;
+  onOpenChange: (open: boolean) => void;
+  title: string;
+};
+
+export default function Dialog({
+  cancelText,
+  children,
+  confirmText,
+  onConfirm,
+  onOpenChange,
+  open,
+  title,
+}: DialogProps) {
+  return (
+    <ChakraDialog.Root
+      lazyMount
+      onOpenChange={(e) => onOpenChange(e.open)}
+      open={open}
+    >
+      <Portal>
+        <ChakraDialog.Backdrop />
+        <ChakraDialog.Positioner>
+          <ChakraDialog.Content>
+            <ChakraDialog.Header>
+              <ChakraDialog.Title>{title}</ChakraDialog.Title>
+            </ChakraDialog.Header>
+
+            <ChakraDialog.Body>{children}</ChakraDialog.Body>
+
+            <ChakraDialog.Footer>
+              <ChakraDialog.ActionTrigger asChild>
+                <Button onClick={() => onOpenChange(false)} variant="outline">
+                  {cancelText}
+                </Button>
+              </ChakraDialog.ActionTrigger>
+
+              <Button onClick={onConfirm}>{confirmText}</Button>
+            </ChakraDialog.Footer>
+
+            <ChakraDialog.CloseTrigger asChild>
+              <CloseButton size="sm" />
+            </ChakraDialog.CloseTrigger>
+          </ChakraDialog.Content>
+        </ChakraDialog.Positioner>
+      </Portal>
+    </ChakraDialog.Root>
+  );
+}
