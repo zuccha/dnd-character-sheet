d4532cee4fc67b3ca89b7f55ac3c339edc57db27
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-12-12 13:36:01 +0100

    feat: add meta data id, version, and name fields to character

--- src/character/character.ts
+++ src/character/character.ts
@@ -5,6 +5,10 @@ import z from "zod";
 //------------------------------------------------------------------------------
 
 export const characterSchema = z.object({
+  _id: z.uuid().default(() => crypto.randomUUID()),
+  _name: z.string().default(""),
+  _version: z.number().default(1),
+
   level: z.number().default(1),
   name: z.string().default(""),
   title: z.string().default(""),
