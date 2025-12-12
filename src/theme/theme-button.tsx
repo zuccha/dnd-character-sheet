878a6f353560e703ee8d4d048b3131a6876ab210
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-12-12 16:59:42 +0100

    feat: allow to pass props to theme button

--- src/theme/theme-button.tsx
+++ src/theme/theme-button.tsx
@@ -1,20 +1,22 @@
 import { MoonIcon, SunIcon } from "lucide-react";
-import IconButton from "~/ui/icon-button";
+import IconButton, { type IconButtonProps } from "~/ui/icon-button";
 import useTheme from "./use-theme";
 
 //------------------------------------------------------------------------------
 // Theme Button
 //------------------------------------------------------------------------------
 
-export default function ThemeButton() {
+export type ThemeButtonProps = Omit<IconButtonProps, "Icon" | "onClick">;
+
+export default function ThemeButton(props: ThemeButtonProps) {
   const [theme, _setColorMode, toggleTheme] = useTheme();
+
   return (
     <IconButton
       Icon={theme === "dark" ? MoonIcon : SunIcon}
       aria-label="Toggle color mode"
       onClick={toggleTheme}
-      size="sm"
-      variant="ghost"
+      {...props}
     />
   );
 }
