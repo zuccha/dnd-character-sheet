399103dd9888a8b08fe2818fdaa6a3a8bf46c98c
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-12-12 16:03:46 +0100

    feat: implement create and remove characters

--- src/character/characters-metadata.ts
+++ src/character/characters-metadata.ts
@@ -1,19 +1,63 @@
 import { useCallback } from "react";
+import z from "zod";
+import { createLocalStore } from "~/store/local-store";
 import { createMemoryStore } from "~/store/memory-store";
-import type { CharacterMetadata } from "./character";
+import {
+  type Character,
+  type CharacterMetadata,
+  characterSchema,
+  defaultCharacter,
+} from "./character";
+
+//------------------------------------------------------------------------------
+// Character Local Storage
+//------------------------------------------------------------------------------
+
+const storageId = (id: string) => `character[${id}]`;
+
+function loadCharacter(id: string): Character {
+  try {
+    const value = localStorage.getItem(storageId(id));
+    if (value === null) return defaultCharacter;
+    return characterSchema.parse(JSON.parse(value));
+  } catch {
+    localStorage.removeItem(id);
+    return defaultCharacter;
+  }
+}
+
+function saveCharacter(
+  id: string,
+  valueOrAction: Character | ((value: Character) => void),
+): void {
+  const value =
+    typeof valueOrAction === "function" ?
+      valueOrAction(loadCharacter(id))
+    : valueOrAction;
+  localStorage.setItem(storageId(id), JSON.stringify(value));
+}
+
+function clearCharacter(id: string): void {
+  localStorage.removeItem(storageId(id));
+}
+
+//------------------------------------------------------------------------------
+// Character Ids Store
+//------------------------------------------------------------------------------
+
+const characterIdsStore = createLocalStore(
+  "character.ids",
+  [],
+  z.array(z.uuid()).parse,
+);
 
 //------------------------------------------------------------------------------
 // Character Metadata Store
 //------------------------------------------------------------------------------
 
-// TODO: Load from local storage.
-const characterMetadataStore = createMemoryStore<CharacterMetadata[]>([
-  { displayName: "Doranakan", id: crypto.randomUUID(), version: 1 },
-  { displayName: "Faith", id: crypto.randomUUID(), version: 1 },
-  { displayName: "Jorvahn", id: crypto.randomUUID(), version: 1 },
-  { displayName: "Lyrha", id: crypto.randomUUID(), version: 1 },
-  { displayName: "Taren", id: crypto.randomUUID(), version: 1 },
-]);
+const characterMetadataStore = createMemoryStore<CharacterMetadata[]>(
+  characterIdsStore.get().map((id) => loadCharacter(id).meta),
+);
 
 //------------------------------------------------------------------------------
 // Use Character Metadata
@@ -34,7 +78,11 @@ export function useCharacterMetadata(): [
   const [metadata] = characterMetadataStore.use();
 
   const createCharacter = useCallback((displayName: string) => {
-    console.log("createCharacter", displayName); // TODO
+    const character = characterSchema.parse({ meta: { displayName } });
+    characterIdsStore.set((prev) => [...prev, character.meta.id]);
+    characterMetadataStore.set((prev) => [...prev, character.meta]);
+    saveCharacter(character.meta.id, character);
+    // TODO: Set as active character.
   }, []);
 
   const exportAllCharactersToJson = useCallback(() => {
@@ -54,11 +102,17 @@ export function useCharacterMetadata(): [
   }, []);
 
   const removeAllCharacters = useCallback(() => {
-    console.log("removeAllCharacters"); // TODO
+    characterIdsStore.get().forEach(clearCharacter);
+    characterIdsStore.set([]);
+    characterMetadataStore.set([]);
+    // TODO: Handle empty active character.
   }, []);
 
   const removeCharacter = useCallback((id: string) => {
-    console.log("removeCharacter", id); // TODO
+    clearCharacter(id);
+    characterIdsStore.set((prev) => prev.filter((other) => other !== id));
+    characterMetadataStore.set((prev) => prev.filter((meta) => meta.id !== id));
+    // TODO: Change active character if the removed one was active.
   }, []);
 
   return [
