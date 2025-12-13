280eebd2bb61a246d97782f51962ee9235b9c970
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-12-13 22:07:25 +0100

    fix: set correct value for h3 font size in theme system

--- src/theme/theme-system.tsx
+++ src/theme/theme-system.tsx
@@ -25,7 +25,7 @@ const themeConfig = defineConfig({
       fontSizes: {
         "cs.h1": { value: "2em" },
         "cs.h2": { value: "1.5em" },
-        "cs.h3": { value: "1.5em" },
+        "cs.h3": { value: "1.3em" },
         "cs.h4": { value: "1.1em" },
         "cs.value.md": { value: "1.7em" },
       },
