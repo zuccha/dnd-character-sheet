330bd45bff4165fd3d7ff9d62e93635145f850c4
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-12-14 01:47:42 +0100

    feat: allow changing death saves in sheet

--- src/views/character-sheet-view/fields/character-sheet-death-saving-throws.tsx
+++ src/views/character-sheet-view/fields/character-sheet-death-saving-throws.tsx
@@ -1,8 +1,12 @@
-import { Box, HStack } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
+import {
+  useActiveCharacterDeathSaveThrowFailures,
+  useActiveCharacterDeathSaveThrowSuccesses,
+} from "~/character/active-character";
 import { useI18nLangContext } from "~/i18n/i18n-lang-context";
-import CheckboxEmptyIcon from "~/icons/checkbox-empty-icon";
 import HeartIcon from "~/icons/heart-icon";
 import SkullIcon from "~/icons/skull-icon";
+import Checkbox from "~/ui/checkbox";
 import Frame, { type FrameProps } from "../frame";
 
 //------------------------------------------------------------------------------
@@ -16,56 +20,73 @@ export default function CharacterSheetDeathSavingThrows(
 ) {
   const { t } = useI18nLangContext(i18nContext);
 
+  const [failures, setFailures] = useActiveCharacterDeathSaveThrowFailures();
+  const [successes, setSuccesses] = useActiveCharacterDeathSaveThrowSuccesses();
+
+  const failure = (index: 0 | 1 | 2) => (checked: boolean) =>
+    setFailures((prevFailures) => {
+      const nextFailures: [boolean, boolean, boolean] = [...prevFailures];
+      nextFailures[index] = checked;
+      return nextFailures;
+    });
+
+  const success = (index: 0 | 1 | 2) => (checked: boolean) =>
+    setSuccesses((prevSuccesses) => {
+      const nextSuccesses: [boolean, boolean, boolean] = [...prevSuccesses];
+      nextSuccesses[index] = checked;
+      return nextSuccesses;
+    });
+
   return (
     <Frame align="flex-start" title={t("death_saving_throws.label")} {...props}>
       <HStack gap={1}>
-        <SuccessIcon />
-        <SuccessIcon />
-        <SuccessIcon />
-        <FailureIcon />
-        <FailureIcon />
-        <FailureIcon />
+        <SuccessCheckbox checked={successes[0]} onValueChange={success(0)} />
+        <SuccessCheckbox checked={successes[1]} onValueChange={success(1)} />
+        <SuccessCheckbox checked={successes[2]} onValueChange={success(2)} />
+        <FailureCheckbox checked={failures[0]} onValueChange={failure(0)} />
+        <FailureCheckbox checked={failures[1]} onValueChange={failure(1)} />
+        <FailureCheckbox checked={failures[2]} onValueChange={failure(2)} />
       </HStack>
     </Frame>
   );
 }
 
 //------------------------------------------------------------------------------
-// Failure Icon
+// Failure Checkbox
 //------------------------------------------------------------------------------
 
-function FailureIcon() {
+type FailureCheckboxProps = {
+  checked: boolean;
+  onValueChange: (checked: boolean) => void;
+};
+
+function FailureCheckbox({ checked, onValueChange }: FailureCheckboxProps) {
   return (
-    <Box position="relative">
-      <SkullIcon
-        h="cs.checkbox"
-        left={0}
-        opacity={0.2}
-        position="absolute"
-        w="cs.checkbox"
-      />
-      <CheckboxEmptyIcon h="cs.checkbox" w="cs.checkbox" />
-    </Box>
+    <Checkbox checked={checked} onValueChange={onValueChange} size="sm">
+      <SkullIcon h="cs.checkbox" opacity={0.2} w="cs.checkbox" />
+    </Checkbox>
   );
 }
 
 //------------------------------------------------------------------------------
-// Success Icon
+// Success Checkbox
 //------------------------------------------------------------------------------
 
-function SuccessIcon() {
+type SuccessCheckboxProps = {
+  checked: boolean;
+  onValueChange: (checked: boolean) => void;
+};
+
+function SuccessCheckbox({ checked, onValueChange }: SuccessCheckboxProps) {
   return (
-    <Box position="relative">
+    <Checkbox checked={checked} onValueChange={onValueChange} size="sm">
       <HeartIcon
         color="fg.error"
         h="cs.checkbox"
-        left={0}
         opacity={0.2}
-        position="absolute"
         w="cs.checkbox"
       />
-      <CheckboxEmptyIcon h="cs.checkbox" w="cs.checkbox" />
-    </Box>
+    </Checkbox>
   );
 }
 
